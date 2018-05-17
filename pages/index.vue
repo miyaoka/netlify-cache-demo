e8e33a1e3800a4774a58a9b04836fdf5108e1e69
Masaya Kazama <miyan@t-p.jp>
2018-05-17 16:59:54 +0900

    Use nuxt

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Cache test</h1>
+
+    <img src="/img1.jpg" width="500">
+    <img src="/img2.jpg" width="500">
+
+    <div>
+      Photo:
+      <a href="https://www.nasa.gov/content/goddard/hubble-s-high-definition-panoramic-view-of-the-andromeda-galaxy" target="_blank">Hubble's High-Definition Panoramic View of the Andromeda</a>
+    </div>
+  </div>
+</template>
